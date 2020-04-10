096b0cdc74e71c598d81fad00276f9b5fd8c88f6
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-04-10 12:18:21 -0300

    30 days of code - day 12 - solution

--- 30_days_of_code/day_12.py
+++ 30_days_of_code/day_12.py
@@ -19,11 +19,29 @@ class Student(Person):
     #   scores - An array of integers denoting the Person's test scores.
     #
     # Write your constructor here
+    def __init__(self, firstName, lastName, idNumber, scores):
+        Person.__init__(self, firstName, lastName, idNumber)
+        self.scores = scores
 
     #   Function Name: calculate
     #   Return: A character denoting the grade.
     #
     # Write your function here
+    def calculate(self):
+        average = sum(self.scores) / len(self.scores)
+        average = int(average)  # avoid floats since ranges below have step=1
+        grade_scale = {'O': range(90, 101),
+                       'E': range(80, 90),
+                       'A': range(70, 80),
+                       'P': range(55, 70),
+                       'D': range(40, 55),
+                       'T': range(0, 40)}
+        for key, values in grade_scale.items():
+            if average in values:
+                grade = key
+                break
+
+        return grade
 
 
 line = input().split()
