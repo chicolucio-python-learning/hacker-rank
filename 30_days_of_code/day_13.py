ddeab543c718e3bf51d1149f9e7a25f55410f0d1
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2020-04-11 12:18:57 -0300

    30 days of code - day 13 - solution

--- 30_days_of_code/day_13.py
+++ 30_days_of_code/day_13.py
@@ -12,6 +12,17 @@ class Book(object, metaclass=ABCMeta):
 # Write MyBook class
 
 
+class MyBook(Book):
+    def __init__(self, title, author, price):
+        super().__init__(title, author)
+        self.price = price
+
+    def display(self):
+        print('Title:', self.title)
+        print('Author:', self.author)
+        print('Price:', self.price)
+
+
 title = input()
 author = input()
 price = int(input())
